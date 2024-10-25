e429ed8943e423b3c6baaf039d216e31736b0c1a
daniji09 <daniji09@ucm.es>
2024-10-26 01:24:15 +0200

    Comprobación de red al buscar implementada

--- Practica2-Libreria/app/src/main/java/es/ucm/fdi/gogglebooksclient/MainActivity.java
+++ Practica2-Libreria/app/src/main/java/es/ucm/fdi/gogglebooksclient/MainActivity.java
@@ -1,5 +1,8 @@
 package es.ucm.fdi.gogglebooksclient;
 
+import android.content.Context;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +19,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.loader.app.LoaderManager;
 
+import android.net.ConnectivityManager;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int BOOK_LOADER_ID = 1;
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton selectedButton;
 
     private final BookLoaderCallbacks bookLoaderCallbacks = new BookLoaderCallbacks(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,12 +57,11 @@ public class MainActivity extends AppCompatActivity {
         radiogroup.setOnCheckedChangeListener((group, checkedId) -> {
             selectedButton = findViewById(checkedId);
             //Restringimos los editText para meter autor o titulo dependiendo del radioGroup que este seleccionado
-            if(selectedButton == findViewById(R.id.radioButtonRevista)){
+            if (selectedButton == findViewById(R.id.radioButtonRevista)) {
                 editTextAutor.setVisibility(View.GONE);
                 editTextAutor.setText("");
                 editTextAutor.setEnabled(false);
-            }
-            else{
+            } else {
                 editTextAutor.setVisibility(View.VISIBLE);
                 editTextAutor.setEnabled(true);
             }
@@ -68,49 +73,65 @@ public class MainActivity extends AppCompatActivity {
         // Listener del botón de búsqueda que, al hacer clic, llama al método searchBooks
         ImageButton busqueda = findViewById(R.id.imageButton);
         busqueda.setOnClickListener(v -> {
-            printType = selectedButton.getText().toString();
-            Log.i("printType", printType);
-            
-            // Modificamos la queryString dependiendo del botón seleccionado
-            if (selectedButton == findViewById(R.id.radioButtonRevista)) {//Revista
-                if(editTextTitulo.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, R.string.error_falta_titulo_revista, Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                queryString = "intitle:" + editTextTitulo.getText().toString();
-            } else { //Libro o ambas
-                // Comprobamos que los campos no estén vacíos
-                Log.i(this.getClass().getName(), "Entro a libros");
-                if (editTextTitulo.getText().toString().isEmpty() && editTextAutor.getText().toString().isEmpty()) {
-                    Log.i(this.getClass().getName(), "Entro a libros");
-                    Toast.makeText(MainActivity.this, R.string.error_text, Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(!editTextTitulo.getText().toString().isEmpty()){
-                    queryString = "intitle:" + editTextTitulo.getText().toString();
-                }
-                if(!editTextAutor.getText().toString().isEmpty()){
-                    if(!queryString.isEmpty()){
-                        queryString += "+inauthor:" + editTextAutor.getText().toString();
-                    }else{
-                        queryString = "inauthor:" + editTextAutor.getText().toString();
+            // la búsqueda se reaiza solo si hay conexión a Internet
+            if (isConnected()) {
+                printType = selectedButton.getText().toString();
+                Log.i("printType", printType);
 
+                // Modificamos la queryString dependiendo del botón seleccionado
+                if (selectedButton == findViewById(R.id.radioButtonRevista)) {//Revista
+                    if (editTextTitulo.getText().toString().isEmpty()) {
+                        Toast.makeText(MainActivity.this, R.string.error_falta_titulo_revista, Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    queryString = "intitle:" + editTextTitulo.getText().toString();
+                } else { //Libro o ambas
+                    // Comprobamos que los campos no estén vacíos
+                    Log.i(this.getClass().getName(), "Entro a libros");
+                    if (editTextTitulo.getText().toString().isEmpty() && editTextAutor.getText().toString().isEmpty()) {
+                        Log.i(this.getClass().getName(), "Entro a libros");
+                        Toast.makeText(MainActivity.this, R.string.error_text, Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if (!editTextTitulo.getText().toString().isEmpty()) {
+                        queryString = "intitle:" + editTextTitulo.getText().toString();
+                    }
+                    if (!editTextAutor.getText().toString().isEmpty()) {
+                        if (!queryString.isEmpty()) {
+                            queryString += "+inauthor:" + editTextAutor.getText().toString();
+                        } else {
+                            queryString = "inauthor:" + editTextAutor.getText().toString();
+
+                        }
                     }
                 }
+                //Llamamos al método searchBooks
+                searchBooks(v);
+                queryString = "";
+            } else {
+                Toast.makeText(MainActivity.this, R.string.error_sin_conexion_busqueda, Toast.LENGTH_SHORT).show();
             }
-            //Llamamos al método searchBooks
-            searchBooks(v);
-            queryString = "";
         });
 
 
     }
 
-    public void searchBooks (View view){
+    public void searchBooks(View view) {
         Bundle queryBundle = new Bundle();
         queryBundle.putString(BookLoaderCallbacks.EXTRA_QUERY, queryString);
         queryBundle.putString(BookLoaderCallbacks.EXTRA_PRINT_TYPE, printType);
         LoaderManager.getInstance(this).restartLoader(BOOK_LOADER_ID,
                 queryBundle, bookLoaderCallbacks);
+    }
+
+    // se comprueba si hay conexion a Internet para poder realizar la busqueda
+    private boolean isConnected() {
+        Log.i(this.getClass().getName(), "Se comprueba la conexión a Internet antes de realizar la búsqueda.");
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connMgr.getActiveNetwork();
+        NetworkCapabilities networkCap =
+                connMgr.getNetworkCapabilities(network);
+        return (networkCap != null);
     }
 }
